52832865252ce5ef0cb0f09b9ba7b4248c9b1fd3
Ruby Shot <domochoice@yahoo.com>
2013-07-14 01:42:56 +0000

    Add index.html & modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs')
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res){
+  var html = fs.readFieSync(htmlfile).toString();
+  res.send(html);
 });
 
 var port = process.env.PORT || 5000;
