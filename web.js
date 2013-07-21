12cb112b1239f095a87c98486c5ce3e77c37853a
Ruby Shot <domochoice@yahoo.com>
2013-07-21 16:50:40 +0000

    web.js

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res){
   res.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
